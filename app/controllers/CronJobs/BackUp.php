b45644cc78043fbfa402b1b46979b7471d16e59b
Andrés Pineda <andr3s2@gmail.com>
2014-09-13 17:04:33 -0500

    Part I backup db personalizado part VI

--- app/controllers/CronJobs/BackUp.php
+++ app/controllers/CronJobs/BackUp.php
@@ -13,45 +13,58 @@ class BackUp {
 
     function __construct() {
 
-        $this->ssh = new Net_SSH2('localhost');
+        $this->ssh = new Net_SSH2('168.176.125.196');
         if (!$this->ssh->login('root', 'qwe123admin')) {
             exit('Login Failed in ssh');
         }
-
-        
-      
+       // $this->clean();
+        //exit;
     }
 
     public function generate() {
-        $this->ssh->exec("mysqldump -u root -pqwe123admin lms | zip > {$this->get_full_file_path()}");  
-        $this->list_files();
+        $this->ssh->exec("mysqldump -u root -pqwe123admin lms | zip > {$this->get_full_file_path()}");
+        $this->clean();
     }
 
     private function list_files() {
-        $files =  $this->ssh->exec("ls {$this->get_full_folder_path()}");
-        echo "<pre>";
-        var_dump($files);
-        echo "</pre>";
-        
+        $files = $this->ssh->exec("ls {$this->get_full_folder_path()}");
+        $files = str_replace("\r\n", "\n", $files);
+        return explode("\n", $files);
     }
 
-    private function remove($file) {
-        
+    private function clean() {
+        if ($this->get_number_of_backups() >= 0) {            
+            $cmd = "rm {$this->create_path_to_file($this->get_file_name_id($this->get_last_id_file()))}";
+            echo $cmd;            
+        }
+       
     }
 
     private function get_last_id_file() {
-    //    return $this->id;
+        $files = $this->list_files();
+        $min = 100000;
+        foreach ($files as $file) {
+            $f = explode("-", trim($file));
+            $min = min($min, $f[0]);
+        }
+        return $min;
+    }
+
+    private function get_file_name_id($id) {
+        $files = $this->list_files();
+        foreach ($files as $file) {
+            $f = explode("-", trim($file));
+            if ($f[0] == $id)
+                return $file;
+        }
     }
 
     private function get_number_of_backups() {
-        
+        return count($this->list_files());
     }
-
-   
 
     private function get_full_file_path() {
         return $this->get_full_folder_path() . $this->get_file_name();
-        
     }
 
     private function get_full_folder_path() {
@@ -63,5 +76,8 @@ class BackUp {
         return $id . $this->prefix . date('Y-m-d') . $this->extension;
     }
 
+    private function create_path_to_file($file) {
+        return $this->get_full_folder_path() . $file;
+    }
 
 }
